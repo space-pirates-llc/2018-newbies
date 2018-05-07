c9d9331360774aa777a06c937c6c3d48ba89dd5c
Junya Ogasawara <ogasawara.junya@moneyforward.co.jp>
2018-05-07 13:27:15 +0900

    Deploy ready

--- nova/scripts/install.sh
+++ nova/scripts/install.sh
@@ -13,5 +13,6 @@ gem install --no-document bundler
 rbenv rehash
 
 bundle install -j 4 --deployment --path=vendor/bundle --without development test
+bundle exec rake db:create db:migrate
 bundle exec rake assets:precompile
 bundle exec rake assets:clean[5]
