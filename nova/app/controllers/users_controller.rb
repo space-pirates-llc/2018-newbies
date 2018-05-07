2e0122968d0113f6eb73841d911e09c8e04d92ae
毛 若愚 <ruoyu.mao@icloud.com>
2018-05-07 21:58:53 +0900

    modified version one

--- nova/app/controllers/users_controller.rb
+++ nova/app/controllers/users_controller.rb
@@ -11,6 +11,10 @@ class UsersController < ApplicationController
     if @user.persisted?
       self.current_user = @user
       redirect_to dashboard_path
+    elsif @user.save
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render :new, status: :bad_request
     end
--- nova/app/mailers/application_mailer.rb
+++ nova/app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'ruoyu.mao@icloud.com'
   layout 'mailer'
 end
