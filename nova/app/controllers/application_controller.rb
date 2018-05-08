69363557ec9fc39dafdbcbce70ee8d591c393aed
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-05-08 11:38:11 +0900

    flash_typesの追加

--- nova/app/controllers/application_controller.rb
+++ nova/app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  add_flash_types :success, :info, :warning, :danger
   include Loginable
 
   protect_from_forgery with: :exception
