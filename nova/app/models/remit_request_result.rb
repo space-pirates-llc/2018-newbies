770eac3c8925a7b17d3c59a081f65d22487ad814
Junya Ogasawara <ogasawara.junya@moneyforward.co.jp>
2018-05-08 22:20:11 +0900

    Add some validations to RemitRequestResult model

--- nova/app/models/remit_request_result.rb
+++ nova/app/models/remit_request_result.rb
@@ -1,4 +1,16 @@
 class RemitRequestResult < ApplicationRecord
+  RESULT_ACCEPTED = 'accepted'.freeze
+  RESULT_REJECTED = 'rejected'.freeze
+  RESULT_CANCELED = 'canceled'.freeze
+  MIN_REMIT_AMOUNT = 1 # TODO: RemitRequestと共通化
+  MAX_REMIT_AMOUNT = 99_999_999 # TODO: RemitRequestと共通化
+
   belongs_to :user
   belongs_to :target, class_name: 'User'
+
+  validates :amount, presence: true, numericality: { only_integer: true,
+                                                     greater_than_or_equal_to: MIN_REMIT_AMOUNT,
+                                                     less_than_or_equal_to: MAX_REMIT_AMOUNT }
+  validates :result, presence: true,
+    inclusion: { in: [RESULT_ACCEPTED, RESULT_REJECTED, RESULT_CANCELED] }
 end
