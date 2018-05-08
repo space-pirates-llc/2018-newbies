e63b2126e1cbba8a23320500cbf5ab90c4d52a7b
Junya Ogasawara <ogasawara.junya@moneyforward.co.jp>
2018-05-08 21:25:31 +0900

    Create RemitRequestResult model

---
+++ nova/app/models/remit_request_result.rb
@@ -0,0 +1,2 @@
+class RemitRequestResult < ApplicationRecord
+end
---
+++ nova/spec/factories/remit_request_results.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :remit_request_result do
+    user_id 1
+    target_id 1
+    amount 1
+    status 'accepted'
+  end
+end
