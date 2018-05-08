87b176bf3a603a71e3996259cd452be3bdea7a80
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-05-08 10:37:57 +0900

    active_recordのdefault_timezoneをutcに変更

--- nova/config/application.rb
+++ nova/config/application.rb
@@ -22,7 +22,7 @@ module Nova
   class Application < Rails::Application
     # Set time_zone to Tokyo
     config.time_zone = 'Tokyo'
-    config.active_record.default_timezone = :local
+    config.active_record.default_timezone = :utc
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
