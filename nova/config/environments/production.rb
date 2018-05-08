34d7ba32fff7cdf8920e5e93ff8406707ae11172
毛 若愚 <ruoyu.mao@icloud.com>
2018-05-08 13:32:38 +0900

    config mailer in production.rb

--- nova/config/environments/production.rb
+++ nova/config/environments/production.rb
@@ -61,7 +61,13 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
+  host = 'mf2018.youki.io'
+
+  config.action_mailer.default_url_options = { host: host, protocol: 'https'}
+
+  config.action_mailer.perform_caching = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
