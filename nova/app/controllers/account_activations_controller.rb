50637d3aeba5e255fb3d4753c39cdab297911950
yuna iwamura <iwamura.yuna@moneyforward.co.jp>
2018-05-09 14:43:34 +0900

    メール認証した後のurlを変更

--- nova/app/controllers/account_activations_controller.rb
+++ nova/app/controllers/account_activations_controller.rb
@@ -6,7 +6,7 @@ class AccountActivationsController < ApplicationController
       user.activate
       log_in user
       flash[:success] = "Account activated! Please log in."
-      redirect_to login_url
+      redirect_to dashboard_path
     else
       flash[:danger] = "Invalid activation link!"
       redirect_to login_url
