7783c5dde5c9d7b468c8f37f881c74b4af440a6e
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-05-09 16:56:25 +0900

    自分宛のリクエストと存在しないユーザーへのリクエストの検証の追加

--- nova/app/models/remit_request.rb
+++ nova/app/models/remit_request.rb
@@ -7,6 +7,8 @@ class RemitRequest < ApplicationRecord
   validates :amount, numericality: { greater_than: 0, only_integer: true }
   validates :user, presence: true
   validates :target, presence: true
+  validate :validate_equal_user_and_target
+  validate :validate_nonexist_target
 
   scope :outstanding, ->(at = Time.current) { not_accepted(at).not_rejected(at).not_canceled(at) }
   scope :accepted, ->(at = Time.current) { where(RemitRequest.arel_table[:accepted_at].lteq(at)) }
@@ -15,6 +17,18 @@ class RemitRequest < ApplicationRecord
   scope :not_rejected, ->(at = Time.current) { where(rejected_at: nil).or(where(RemitRequest.arel_table[:rejected_at].gt(at))) }
   scope :canceled, ->(at = Time.current) { where(RemitRequest.arel_table[:canceled_at].lteq(at)) }
   scope :not_canceled, ->(at = Time.current) { where(canceled_at: nil).or(where(RemitRequest.arel_table[:canceled_at].gt(at))) }
+
+  def validate_equal_user_and_target
+    if user.email == target.email
+      errors.add(:base, "自分宛にリクエストは作成できません。")
+    end
+  end
+
+  def validate_nonexist_target
+    if User.where(email: target.email).blank?
+      errors.add(:base, "登録されていないユーザーです。")
+    end
+  end
 
   def outstanding?(at = Time.current)
     !accepted?(at) && !rejected?(at) && !canceled?(at)
