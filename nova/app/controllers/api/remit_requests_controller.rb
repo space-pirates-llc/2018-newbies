5b252a858a46e6c8e7229e25e2d9b1ab3f51d453
4geru <uchinishi.koichi@moneyforward.co.jp>
2018-05-09 15:27:10 +0900

    fix counts -> count

--- nova/app/controllers/api/remit_requests_controller.rb
+++ nova/app/controllers/api/remit_requests_controller.rb
@@ -3,7 +3,7 @@
 class Api::RemitRequestsController < Api::ApplicationController
   def index
     @remit_requests = current_user.received_remit_requests.page(params[:page])
-    page_limit = @remit_requests.counts
+    page_limit = @remit_requests.count
     pages = current_user.received_remit_requests.count / page_limit + 
       ( current_user.received_remit_requests.count % page_limit ? 1 : 0)
     render json:{max_pages: pages, remit_requests: @remit_requests.as_json(include: :user).to_a}
