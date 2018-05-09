96f1aa33ec1c029ee873e8cbaebd34e15dc37195
takenaka-kazumasa <takenaka.kazumasa@moneyforward.co.jp>
2018-05-09 12:41:09 +0900

    routesを整理した

--- nova/config/routes.rb
+++ nova/config/routes.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  #竹中が追加したdevise関連のroutes
-  devise_for :users, controllers: {
-      sessions: 'users/sessions', passwords: 'users/passwords', registrations: 'users/registrations'
-  }
+  devise_scope :user do
+    get 'users/sign_in', to: 'users/sessions#new', as: 'new_user_session'
+    post 'users/sign_in', to: 'users/sessions#create', as: 'user_session'
+    delete 'users/sign_out', to: 'users/sessions#destroy', as: 'destroy_user_session'
+    get 'users/sign_up', to: 'users/registrations#new', as: 'new_user_registration'
+    post 'users', to: 'users/registrations#create', as: 'user_registration'
+  end
 
   namespace :api, defaults: { format: 'json' } do
     resource :user, only: %i[show update]
@@ -19,11 +22,6 @@ Rails.application.routes.draw do
     end
   end
   get '/dashboard', to: 'dashboard#show'
-  #get '/signup', to: 'users#new'
-  #post '/signup', to: 'users#create'
-  #get '/login', to: 'sessions#new', as: 'login'
-  #post '/login', to: 'sessions#create'
-  #get '/logout', to: 'sessions#destroy'
 
   root to: 'pages#root'
 end
