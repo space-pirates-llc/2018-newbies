beb6cc287836c27811be03db579c0cc48aa57b6e
takenaka-kazumasa <takenaka.kazumasa@moneyforward.co.jp>
2018-05-10 19:09:10 +0900

    test通る

--- nova/spec/support/loginable.rb
+++ nova/spec/support/loginable.rb
@@ -1,23 +1,23 @@
 # frozen_string_literal: true
 
-module RSpec::LoginHelper
-  protected
-
-  def login!(user = nil)
-    user ||= FactoryBot.create(:user)
-    self.current_user = user
-    user
-  end
-
-  def current_user
-    User.find_by(id: cookies[Loginable::COOKIE_NAME])
-  end
-
-  def current_user=(user)
-    cookies[Loginable::COOKIE_NAME] = user&.id
-  end
-end
-
-RSpec.configure do |config|
-  config.include(RSpec::LoginHelper, type: :controller)
-end
+#module RSpec::LoginHelper
+#  protected
+#
+#  def sign_in(user = nil)
+#    user ||= FactoryBot.create(:user)
+#    self.current_user = user
+#    user
+#  end
+#
+#  def current_user
+#    User.find_by(id: cookies[Loginable::COOKIE_NAME])
+#  end
+#
+#  def current_user=(user)
+#    cookies[Loginable::COOKIE_NAME] = user&.id
+#  end
+#end
+#
+#RSpec.configure do |config|
+#  config.include(RSpec::LoginHelper, type: :controller)
+#end
