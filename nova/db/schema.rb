128846431410b2ae849d3cf305dc1f0134d9b2b6
kanebest <kanetsuki.ryoma@moneyforward.co.jp>
2018-05-10 11:17:37 +0900

    カラム名をbalance→amountに変更

--- nova/db/schema.rb
+++ nova/db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2018_05_10_015320) do
 
   create_table "balances", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.integer "balance", default: 0
+    t.integer "amount", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -60,7 +60,6 @@ ActiveRecord::Schema.define(version: 2018_05_10_015320) do
     t.boolean "staff", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "balance", default: 0
   end
 
   add_foreign_key "charges", "users"
--- nova/spec/factories/balances.rb
+++ nova/spec/factories/balances.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :balance do
     user_id ""
-    balance 1
+    amount 1
   end
 end
