071ffe2c3d567748baf9af03d60e0740ef566269
kanebest <kanetsuki.ryoma@moneyforward.co.jp>
2018-05-10 11:17:37 +0900

    カラム名をbalance→amountに変更

--- nova/db/migrate/20180510015320_create_balances.rb
+++ nova/db/migrate/20180510015320_create_balances.rb
@@ -2,7 +2,7 @@ class CreateBalances < ActiveRecord::Migration[5.2]
   def change
     create_table :balances do |t|
       t.bigint :user_id, null: false
-      t.integer :balance, default: 0
+      t.integer :amount, default: 0
 
       t.timestamps
     end
