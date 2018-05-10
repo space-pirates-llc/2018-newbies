5cff13bf15661bb82722d467c368daee8df58e61
takenaka-kazumasa <takenaka.kazumasa@moneyforward.co.jp>
2018-05-10 15:20:33 +0900

    csrf対策を有効にした

--- nova/app/controllers/application_controller.rb
+++ nova/app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 
 class ApplicationController < ActionController::Base
   #include Loginable
-  #protect_from_forgery with: :exception
+  protect_from_forgery with: :exception, prepend: true
 
   def after_sign_in_path_for(users)
     dashboard_path
