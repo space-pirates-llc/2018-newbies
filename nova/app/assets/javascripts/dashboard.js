1c3d0f02dcccf6def8b7e642de0701b182898039
Takuya Nandate <nandate.takuya@moneyforward.co.jp>
2018-05-10 17:30:54 +0900

    json.errorsの有無によってメッセージを変える

--- nova/app/assets/javascripts/dashboard.js
+++ nova/app/assets/javascripts/dashboard.js
@@ -213,6 +213,11 @@ document.addEventListener('DOMContentLoaded', function() {
         var self = this;
         api.put('/api/user?attribute=nickname', { user: this.user }).
           then(function(json) {
+            if(json.errors){
+                self.setting_notice = "更新に失敗しました"
+            }else{
+                self.setting_notice = "更新しました"
+            }
             self.user = json;
           });
       },
@@ -222,6 +227,11 @@ document.addEventListener('DOMContentLoaded', function() {
         var self = this;
         api.put('/api/user?attribute=email', { user: this.user }).
           then(function(json) {
+            if(json.errors){
+              self.setting_notice = "更新に失敗しました"
+            }else{
+              self.setting_notice = "更新しました"
+            }
             self.user = json;
           });
       },
@@ -231,6 +241,11 @@ document.addEventListener('DOMContentLoaded', function() {
         var self = this;
         api.put('/api/user?attribute=password', { user: this.user }).
           then(function(json) {
+            if(json.errors){
+                self.setting_notice = "更新に失敗しました"
+            }else{
+                self.setting_notice = "更新しました"
+            }
             self.user = json;
           });
       },
