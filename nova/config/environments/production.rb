c621deaf8994dba1f21570310476e6b6cd0b6b0a
furuhama <furuhama@users.noreply.github.com>
2018-05-11 11:45:51 +0900

    Add aws-sdk gem

--- nova/config/environments/production.rb
+++ nova/config/environments/production.rb
@@ -63,6 +63,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
 
+  config.action_mailer.delivery_method = :aws_sdk
+
   host = 'mf2018.youki.io'
 
   config.action_mailer.default_url_options = { host: host, protocol: 'https'}
