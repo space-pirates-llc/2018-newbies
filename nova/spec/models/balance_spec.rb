203fa57c24c232211fb273a785d41515b809ecae
kanebest <kanetsuki.ryoma@moneyforward.co.jp>
2018-05-11 12:52:13 +0900

    balanceのtestcodeを追加

--- nova/spec/models/balance_spec.rb
+++ nova/spec/models/balance_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Balance, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'factory validation' do
+    subject(:balance) { create(:user) }
+
+    it { is_expected.to be_valid }
+  end
+
+  describe 'Association' do
+    it { is_expected.to belong_to(:user) }
+  end
+
+  describe 'Validation' do
+    subject(:balance) { create(:user).balance }
+
+    describe 'amount' do
+      it { is_expected.to validate_presence_of(:amount) }
+      it { is_expected.to validate_numericality_of(:amount).is_greater_than(0).is_less_than(100000) }
+    end
+  end
 end
