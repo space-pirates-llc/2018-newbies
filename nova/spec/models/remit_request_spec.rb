c5c5cc0aa23eb357e5aa994a873429a4756cada7
kanebest <kanetsuki.ryoma@moneyforward.co.jp>
2018-05-11 14:55:58 +0900

    10万を超えるチャージ時のボタン導線を削除

--- nova/spec/models/remit_request_spec.rb
+++ nova/spec/models/remit_request_spec.rb
@@ -35,7 +35,7 @@ RSpec.describe RemitRequest, type: :model do
   describe 'Validations' do
     describe 'amount' do
       subject(:remit_request) { build(:remit_request) }
-      it { is_expected.to validate_numericality_of(:amount).is_greater_than(0).only_integer }
+      it { is_expected.to validate_numericality_of(:amount).is_greater_than(0).is_less_than(100000).only_integer }
     end
   end
 end
