f24b7376fdd28bb1d0d5173dfdb942758ececa3c
Junya Ogasawara <ogasawara.junya@moneyforward.co.jp>
2018-05-14 12:26:51 +0900

    Pessimistic lock for remit requet in remit service

--- nova/app/services/remit_service.rb
+++ nova/app/services/remit_service.rb
@@ -9,15 +9,22 @@ class RemitService
         user_balance = remit_request.user.balance
         requested_user_balance = remit_request.requested_user.balance
 
-        # balanceの整合性を担保するため、悲観的ロックをかける
-        aquire_lock!([user_balance, requested_user_balance])
+        # 整合性を担保するため、悲観的ロックをかける
+        # # 悲観的ロックの理由
+        # ## Balance
+        # 送金処理中に残高が減ってしまい、本来送金できるべきではないのに送金できてしまうことを防ぐため
+        #
+        # ## RemitRequest
+        # 送金処理中にRemitRequestが削除等され、RemitRequestが存在しない状態で送金処理が行われないようにするため
+        lock_targets = [remit_request, user_balance, requested_user_balance]
+        aquire_lock!(lock_targets)
 
         raise InsufficientBalanceError unless can_remit?(requested_user_balance, remit_request.amount)
 
         transfer_balance!(user_balance, requested_user_balance, remit_request.amount)
         finalize_remit_request!(remit_request)
 
-        release_lock!([user_balance, requested_user_balance])
+        release_lock!(lock_targets)
       end
     end
 
@@ -35,13 +42,13 @@ class RemitService
       remit_request.destroy!
     end
 
-    # balanceの整合性を担保するため悲観的行ロックを獲得する
+    # 整合性を担保するため悲観的行ロックを獲得する
     # デッドロックを防ぐため、id昇順にロックを獲得していく
     def aquire_lock!(balances)
       balances.sort_by(&:id).each(&:lock!)
     end
 
-    # balanceの整合性を担保するため悲観的行ロックを開放する
+    # 整合性を担保するため悲観的行ロックを開放する
     # デッドロックを防ぐため、aquire_lock!とは逆順で行ロックを開放する
     def release_lock!(balances)
       balances.sort_by(&:id).reverse.each(&:save!)
