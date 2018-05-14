26d117af73e84d5526cc1a5395904369868d0542
Junya Ogasawara <ogasawara.junya@moneyforward.co.jp>
2018-05-14 17:23:28 +0900

    Add sidekiq configuration and fix scripts

--- nova/scripts/start.sh
+++ nova/scripts/start.sh
@@ -8,4 +8,4 @@ cd /var/nova/current
 export RAILS_ENV=production
 
 bundle exec puma -d -e production -C config/puma.rb
-bundle exec sidekiq -d -e production
+bundle exec sidekiq -d -e production -P /var/nova/current/tmp/pids/sidekiq.pid -L /var/nova/current/log/sidekiq.log
--- nova/scripts/stop.sh
+++ nova/scripts/stop.sh
@@ -5,3 +5,7 @@ set -e
 if [ -f /var/nova/current/tmp/pids/puma.pid ]; then
   su -l ubuntu -c 'kill -s TERM `cat /var/nova/current/tmp/pids/puma.pid`'
 fi
+
+if [ -f /var/nova/current/tmp/pids/sidekiq.pid ]; then
+  su -l ubuntu -c 'kill -s TERM `cat /var/nova/current/tmp/pids/sidekiq.pid`'
+fi
