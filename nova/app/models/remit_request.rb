d03f971ddec921a3c2f876de327063f3fd7d49e8
furuhama <furuhama@users.noreply.github.com>
2018-05-14 15:42:00 +0900

    Add `errored` status & Rescue AR::RecordInvalid error & List somewhere

--- nova/app/models/remit_request.rb
+++ nova/app/models/remit_request.rb
@@ -10,23 +10,23 @@ class RemitRequest < ApplicationRecord
   validates :amount, numericality: { greater_than_or_equal_to: MIN_REMIT_AMOUNT, less_than_or_equal_to: MAX_REMIT_AMOUNT, only_integer: true }, presence: true
   validate :validate_equal_user_and_target
   validate :validate_nonexist_target
-  enum status: { outstanding: 0, accepted: 1, rejected: 2, canceled: 3 }
+  enum status: { outstanding: 0, accepted: 1, rejected: 2, canceled: 3, errored: 4 }
 
   # remit_requestモデルが作成された時の状態は outstanding
-  # そこから１方向に変化する。(accepted, rejected, canceledのどれか)
+  # そこから１方向に変化する。(accepted, rejected, canceled, errored のどれか)
   # 2度以上状態が変化しない。
 
   private
 
   def validate_equal_user_and_target
     if user.email == target.email
-      errors.add(:base, "自分宛にリクエストは作成できません。")
+      errors.add(:base, '自分宛にリクエストは作成できません。')
     end
   end
 
   def validate_nonexist_target
     if User.where(email: target.email).blank?
-      errors.add(:base, "登録されていないユーザーです。")
+      errors.add(:base, '登録されていないユーザーです。')
     end
   end
 end
