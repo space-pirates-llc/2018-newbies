815918ac3a0fb434479f2eeb8fdf330921398ef1
takenaka-kazumasa <takenaka.kazumasa@moneyforward.co.jp>
2018-05-14 17:15:06 +0900

    enable to pass test

--- nova/spec/factories/users.rb
+++ nova/spec/factories/users.rb
@@ -5,5 +5,6 @@ FactoryBot.define do
     nickname { FFaker::Internet.user_name }
     email { FFaker::Internet.email }
     password { FFaker::Internet.password }
+    confirmed_at { Time.now }
   end
 end
