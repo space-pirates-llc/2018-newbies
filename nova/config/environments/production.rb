40be17da31578a50d0736d9ab311c692942ef995
takenaka-kazumasa <takenaka.kazumasa@moneyforward.co.jp>
2018-05-14 16:50:28 +0900

    remove unused codes

--- nova/config/environments/production.rb
+++ nova/config/environments/production.rb
@@ -58,6 +58,8 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "nova_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :ses
+  config.action_mailer.perform_deliveries = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
