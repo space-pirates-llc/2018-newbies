dd26f712b86ad1c601c5fa1199b134c23023c73c
kanebest <kanetsuki.ryoma@moneyforward.co.jp>
2018-05-14 12:10:58 +0900

    remove user test from balance_spec

--- nova/spec/models/balance_spec.rb
+++ nova/spec/models/balance_spec.rb
@@ -1,11 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Balance, type: :model do
-  describe 'factory validation' do
-    subject(:user) { create(:user) }
-
-    it { is_expected.to be_valid }
-  end
 
   describe 'Association' do
     it { is_expected.to belong_to(:user) }
