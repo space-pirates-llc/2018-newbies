9cc6d9a5603ffc9bfe7689557abe3c0b0d465e3c
furuhama <furuhama@users.noreply.github.com>
2018-05-15 12:06:34 +0900

    Fix config in production

--- nova/config/environments/production.rb
+++ nova/config/environments/production.rb
@@ -26,7 +26,10 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
+  #
+  # ES6 の文法を許可するために harmony option を true としている
+  config.assets.js_compressor = Uglifier.new(harmony: :true)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
